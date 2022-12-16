1d7fae002a59fc0f9ae277d66f6f4c7f7819b10f
Yosua <yosua.halim@gmail.com>
2022-12-16 21:24:30 +0700

    remove secret

--- pusher.ts
+++ pusher.ts
@@ -4,7 +4,7 @@ import ClientPusher from "pusher-js";
 export const serverPusher = new Pusher({
   appId: "1526010",
   key: "7c2021ff71c620b1bd8f",
-  secret: "9fd2d1bf9cd116ff8f26",
+  secret: process.env.PUSHER_SECRET || "",
   cluster: "ap1",
   useTLS: true,
 });
